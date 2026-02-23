6dfe7f75e1dbfb7ff1ac13ceb5ae256dbe0ab69b
BrunoELuy <brunoluy.e10@gmail.com>
2026-02-23 13:37:47 -0300

    Create Order routes, Create order and add item for order

---
+++ app/presentation/routes/order_routes.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request, jsonify
+from app.infrastructure.database.db import db
+from app.infrastructure.database.models import Order, OrderItem
+
+order_bp = Blueprint("orders", __name__)
+
+@order_bp.route("/orders", methods=["POST"])
+def create_order():
+    data = request.json
+
+    order = Order(user_id=data["user_id"])
+    db.session.add(order)
+    db.session.commit()
+
+    return jsonify({"order_id": order.id}), 201
+
+@order_bp.route("/orders/<int:order_id>/items", methods=["POST"])
+def add_item(order_id):
+    data = request.json
+
+    item = OrderItem(
+        order_id=order_id,
+        product_name=data["product_name"],
+        quantity=data["quantity"],
+        unit_price=data["unit_price"],
+    )
+
+    db.session.add(item)
+
+    items = OrderItem.query.filter_by(oder_id=order_id).all()
+    total = sum(i.quantity * i.unit_price for i in items)
+    total += data["quantity"] * data["unit_price"]
+
+    order = Order.query.get(order_id)
+    order.total_amount = total
+
+    db.session.commit()
+
+    return jsonify({"message": "Item added", "total": total})
