e76c0d0b2d57706c16cb2202c6db026f1ea4a24a
BrunoELuy <brunoluy.e10@gmail.com>
2026-02-24 11:19:09 -0300

    feat(orders): implement authenticated order creation with item validation and total calculation

--- app/presentation/routes/order_routes.py
+++ app/presentation/routes/order_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from sqlalchemy import null
 from app.infrastructure.database.db import db
 from app.infrastructure.database.models import Order, OrderItem
 from app.infrastructure.security.auth_middleware import token_required
@@ -6,14 +7,45 @@ from app.infrastructure.security.auth_middleware import token_required
 order_bp = Blueprint("orders", __name__)
 
 @order_bp.route("/orders", methods=["POST"])
-def create_order():
+@token_required
+def create_order(current_user):
     data = request.json
 
-    order = Order(user_id=data["user_id"])
+    if "items" not in data or not isinstance(data["items"], list) or not data["items"]:
+        return jsonify({"message": "Data error"}), 422
+    else:
+        for item_data in data["items"]:
+            if "quantity" not in item_data or not isinstance(item_data["quantity"], int) or not item_data["quantity"]:
+                return jsonify({"message": "Data error"}), 422
+            elif "unit_price" not in item_data or not isinstance(item_data["unit_price"], (int, float)) or not item_data["unit_price"]:
+                return jsonify({"message": "Data error"}), 422
+            elif "product_name" not in item_data or not isinstance(item_data["product_name"], str) or not item_data["product_name"]:
+                return jsonify({"message": "Data error"}), 422
+
+    order = Order(user_id=current_user.id)
     db.session.add(order)
+    db.session.flush()
+
+    total_amount = 0
+    
+    for item_data in data["items"]:
+        item_quantity = item_data["quantity"]
+        item_price = item_data["unit_price"]
+        item_amount = item_quantity * item_price
+        total_amount += item_amount
+        item = OrderItem(
+            order_id=order.id,
+            product_name=item_data["product_name"],
+            quantity=item_data["quantity"],
+            unit_price=item_data["unit_price"],
+        )
+        db.session.add(item)
+
+    order.total_amount = total_amount
+
     db.session.commit()
 
-    return jsonify({"order_id": order.id}), 201
+    return jsonify({"message": "Item added", "total": total_amount, "order_id": order.id}), 201
 
 
 @order_bp.route("/orders", methods=["GET"])
@@ -39,7 +71,7 @@ def add_item(order_id):
 
     db.session.add(item)
 
-    items = OrderItem.query.filter_by(oder_id=order_id).all()
+    items = OrderItem.query.filter_by(order_id=order_id).all()
     total = sum(i.quantity * i.unit_price for i in items)
     total += data["quantity"] * data["unit_price"]
 
