b3313c3779bb3c18419057b7246cbc808f5a2067
BrunoELuy <brunoluy.e10@gmail.com>
2026-02-26 14:10:02 -0300

    feat(orders): allow authenticated users to update pending orders

--- app/presentation/routes/order_routes.py
+++ app/presentation/routes/order_routes.py
@@ -129,3 +129,43 @@ def delete_order(current_user, order_id):
     db.session.commit()
 
     return jsonify({"message": "Order deleted sucessfully"}), 200
+
+@order_bp.route("/orders/<int:order_id>", methods=["PUT"])
+@token_required
+def update_item_in_orders(current_user, order_id):
+
+    data = request.json
+    try:
+        validate_order_items(data)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 422
+
+    order = Order.query.get(order_id)
+    validation_error = validate_order_ownership(order, current_user)
+    if validation_error:
+        return jsonify(validation_error[0]), validation_error[1]
+    
+    if order.status != "PENDING":
+        return jsonify({"message": "Order cannot be update"}), 422
+
+    order.items.clear()
+
+    total_amount = 0
+    for item_data in data["items"]:
+        item_quantity = item_data["quantity"]
+        item_price = item_data["unit_price"]
+        item_amount = item_quantity * item_price
+        total_amount += item_amount
+        new_item = OrderItem(
+            product_name=item_data["product_name"],
+            quantity=item_data["quantity"],
+            unit_price=item_data["unit_price"],
+        )
+
+        order.items.append(new_item)
+
+    order.total_amount = total_amount
+
+    db.session.commit()
+
+    return jsonify({"message": "Order updated successfully", "total": total_amount})
