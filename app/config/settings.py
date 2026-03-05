9d768a67c77846ab3004e15811105c6476853fdc
unknown <brunoluy.e10@gmail.com>
2026-03-04 22:04:23 -0300

    test(setup): add TestingConfig with in-memory SQLite database

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,3 +2,8 @@ class Config:
     SECRET_KEY = "supersecretkey"
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    TESTING = True
