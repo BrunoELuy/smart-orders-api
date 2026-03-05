0c0ba972391dc6113af9e9a8472c1bc64e4d7107
unknown <brunoluy.e10@gmail.com>
2026-03-04 22:08:04 -0300

    test(setup): enable TestingConfig in create_app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
-from app.config.settings import Config
+from app.config.settings import Config, TestingConfig
 from app.infrastructure.database.db import db
 from app.presentation.routes.auth_routes import auth_bp
 from app.presentation.routes.order_routes import order_bp
 
 
-def create_app():
+def create_app(config_name="default"):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    if config_name == "testing":
+        app.config.from_object(TestingConfig)
+    else:
+        app.config.from_object(Config)
 
     db.init_app(app)
 
